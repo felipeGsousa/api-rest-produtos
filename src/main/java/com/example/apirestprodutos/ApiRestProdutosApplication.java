fcf2170bd0390c42b2aaf7d13d063f87a0ec523e
felipeGsousa <felipe31galdino@gmail.com>
2021-06-27 17:32:17 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/example/apirestprodutos/ApiRestProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProdutosApplication.class, args);
+	}
+
+}
